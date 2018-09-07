98e9bb318e5a76e71798eaedd539ed96f4081ba1
IT <IT@IT-PC18>
2018-09-07 10:44:30 +0530

    commit this message

---
+++ 16H61A1229_Calc_Proj/src/GsDemo.java
@@ -0,0 +1,12 @@
+
+public class GsDemo {
+	int Htno;
+public int getHtno() {
+		return Htno;
+	}
+
+	public void setHtno(int htno) {
+		Htno = htno;
+	}
+}
+                                                                 
